2fad4e8687503041f4dd4dc82d12ea5549e9af85
ed.xyz <eddietang2314@gmail.com>
2023-09-04 10:22:09 -0400

    acadsfdsa

--- lib/src/screens/components.dart
+++ lib/src/screens/components.dart
@@ -129,28 +129,6 @@ class _ComponentsPageState extends State<ComponentsPage> {
                               color: data.isSelected ? Colors.blue : null),
                           onTap: () => selectAllChild(data),
                         ),
-
-                      // Padding(
-                      //   padding: const EdgeInsets.only(right: 5),
-                      //   child: InkWell(
-                      //     splashColor: Colors.amberAccent.withOpacity(1),
-                      //     highlightColor: Colors.red,
-                      //     onTap: () {
-                      //       selectAllChild(data);
-                      //     },
-                      //     child: data.isSelected
-                      //         ? const Icon(
-                      //             Icons.star,
-                      //             size: 30,
-                      //             color: Color(0xFFFF7F50),
-                      //           )
-                      //         : const Icon(
-                      //             Icons.star_border,
-                      //             size: 30,
-                      //             color: Color(0xFFFFDAB9),
-                      //           ),
-                      //   ),
-                      // ),
                       if (data.level == 0)
                         Text(" Building ${data.indexInParent + 1}:")
                       else if (data.level == 1)
@@ -161,17 +139,17 @@ class _ComponentsPageState extends State<ComponentsPage> {
                         Text(" Storage Bin ${data.indexInParent + 1}:")
                       else if (data.level == 4)
                         Text(" Component ${data.indexInParent + 1}:"),
-                      // Text(
-                      //   'level-${data.level}-${data.indexInParent}',
-                      //   // style: TextStyle(
-                      //   //     fontSize: 15, color: getColor(item.level)),
-                      // ),
                       const SizedBox(
                         width: 10,
                       ),
                       Text(
                         data.name.toString().capitalize(),
-                        // style: TextStyle(color: item.color),
+                        style: TextStyle(
+                            color: data.level == 4
+                                ? data.qty > 0
+                                    ? Colors.green
+                                    : Colors.red
+                                : null),
                       ),
                     ],
                   ),
